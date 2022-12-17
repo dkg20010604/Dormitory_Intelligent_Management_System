ac9d1ee6e90f484254242e151b3418a41b657206
dkg20010604 <858943985@qq.com>
2022-12-17 13:27:33 +0800

    添加项目文件。

---
+++ Static_Class/Result_Help/Http_Help.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class.Result_Help
+{
+    public class Http_Help<T> where T : class, new()
+    {
+        public int Code{get; set;}
+        public string? Message{get; set;}
+        public T? data{get; set;}
+    }
+}
---
+++ Static_Class/Result_Help/Http_Helper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Class.Result_Help
+{
+    public class Http_Helper<T> where T : class, new()
+    {
+        public Http_Help<T> Succeed(string message, T value) => new Http_Help<T>() { Code = 200, Message = message, data = value };
+        public Http_Help<T> Warming(string message) => new Http_Help<T>() { Code = 400, Message = message, };
+    }
+}
